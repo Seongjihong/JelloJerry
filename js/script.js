5254af08e09ab14772a415cbe405e65a07fe7427
Seongjihong <ghdekd8216@gmail.com>
2025-04-28 11:09:15 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,33 @@
+$('.1').click(function(){
+    $("body,html").animate({
+        scrollTop:900
+        // 넘어갈 섹션 크기
+    
+    },500)
+    return false;
+  });
+
+  
+  
+  $('.2').click(function(){
+    $("body,html").animate({
+        scrollTop:3450
+        // 넘어갈 섹션 크기
+    },500)
+    return false;
+  });
+  $('.3').click(function(){
+    $("body,html").animate({
+        scrollTop:4320
+        // 넘어갈 섹션 크기
+    },500)
+    return false;
+  });
+  $('.4').click(function(){
+    $("body,html").animate({
+        scrollTop:6900
+        // 넘어갈 섹션 크기
+    },500)
+    return false;
+  
+  });
